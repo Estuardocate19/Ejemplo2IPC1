1902490f3e554d43825de76001620d4171666f56
Estuardo Caté Simón <ecatesimon@gmail.com>
2024-02-15 23:37:41 -0600

    cambios 2

--- ejemplocomandosgithub/src/main/java/com/mycompany/ejemplocomandosgithub/Ejemplocomandosgithub.java
+++ ejemplocomandosgithub/src/main/java/com/mycompany/ejemplocomandosgithub/Ejemplocomandosgithub.java
@@ -12,5 +12,9 @@ public class Ejemplocomandosgithub {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hola mundo");
+        System.out.println("Comprobacion de ejemplo");
+        
+        
     }
 }
